083d4cbd77568144b34cfd6b3db5aa6af291667b
skurkov <p.skurkov@tendertech.ru>
2019-01-28 17:44:56 +0300

    problem-2 reoslved

---
+++ problem-2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+//Problem-2 reverse number without strings or any build in functions
+
+//solution->done
+
+func main() {
+
+	fmt.Print("Enter number: ")
+	reader := bufio.NewScanner(os.Stdin)//New scanner for users number
+	var number int
+	for reader.Scan() {//Start scanning terminal
+		breaker := reader.Text()//Get string from user
+		i, err := strconv.Atoi(breaker)//Just convert string to int
+		if err != nil {//If something going wrong
+			fmt.Println("Oops, something went wrong")
+			continue
+		}
+		if i == 0 {
+			fmt.Println("Enter a number greater than zero:")
+			continue
+		}
+		number = i
+		break
+	}
+	fmt.Printf("Result: %v", inverseNumber(number))
+}
+
+
+func inverseNumber(n int) (i int) {
+	for n != 0 {
+		i *= 10
+		mod := n % 10
+		i += mod
+		n = (n - mod) / 10
+	}
+	return
+}
