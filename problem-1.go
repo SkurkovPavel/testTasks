38538da3ce56bed48af2c8e2d8fd4052289006ec
skurkov <p.skurkov@tendertech.ru>
2019-01-28 17:11:43 +0300

    problem-1 comments

--- problem-1.go
+++ problem-1.go
@@ -13,42 +13,42 @@ import (
 
 func main() {
 	fmt.Print("Enter number: ")
-	reader := bufio.NewScanner(os.Stdin)
+	reader := bufio.NewScanner(os.Stdin)//New scanner for users numbers
 
-	var basicSlice []int
+	var basicSlice []int //Slice for user numbers
 
-	for reader.Scan() {
-		breaker := reader.Text()
-		i, err := strconv.Atoi(breaker)
-		if err != nil {
+	for reader.Scan() { //Start scanning terminal
+		breaker := reader.Text()//Get string from user
+		i, err := strconv.Atoi(breaker)//Just convert string to int
+		if err != nil { //If something going wrong or user finished input
 			fmt.Print("result:")
 			break
 		}
-		basicSlice = append(basicSlice, i)
-		fmt.Print("One more number?:")
-		continue
+		basicSlice = append(basicSlice, i) //Append users numbers into slice
+		fmt.Print("One more number?:") //Just say something for fun
+		continue //Continue scanning
 	}
-	esc := false
-	for !esc  {
-		esc = true
-		for chKey := range basicSlice {
-			if chKey+1 < len(basicSlice) {
-				if basicSlice[chKey] > basicSlice[chKey+1] {
-					esc = false
-					arraySwap(basicSlice, chKey)
-					arraySwap(basicSlice, chKey+1)
-					arraySwap(basicSlice, chKey)
+	esc := false //Flag to exit an infinite loop
+	for !esc { //Star infinite loop
+		esc = true //Set the flag to exit the loop when we finish sorting
+		for chKey := range basicSlice { //Start sorting
+			if chKey+1 < len(basicSlice) { // we need to check "out of range" error
+				if basicSlice[chKey] > basicSlice[chKey+1] {//We need to check n element bigger then n+1
+					esc = false //Set the flag which shown that we had changed something in array
+					arraySwap(basicSlice, chKey)  //sort
+					arraySwap(basicSlice, chKey+1) //sort
+					arraySwap(basicSlice, chKey) //sort
 					continue
 				}
 			}
 
 		}
 	}
-	fmt.Println(basicSlice)
+	fmt.Println(basicSlice) //Print result
 }
 
 func arraySwap(arr []int, n int) {
-
+	//Ыwap array elements
 	arr[n], arr[0] = arr[0], arr[n]
 
 }
